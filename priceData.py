fff8ea2471419c6cc4cb53ee61cf6d8118bfb34c
Roland Afzelius <afzelius@bc.edu>
2021-11-29 20:15:56 -0500

    Updated

--- priceData.py
+++ priceData.py
@@ -7,49 +7,27 @@ import json
 cg = CoinGeckoAPI()
 pApi = 'gvmfl1vyDKqYI6PHEtV4xPCfUmAEYSCu'
 
-def priceDataCG(coins, sDay, sMonth, sYear):
-    days = 1
-    prices = []
-    dates = []
-    for coin in range(0,len(coins)):
-        tmpPrices = []
-        tmpDates = []
-        print(coins[coin])
-        for i in range(0,1):
+
+
+def priceDataCG(coins, sDay, sMonth, sYear, days):
+    df = pd.DataFrame(columns = ['Date',coins[0]])
+    for coin in range(0,1):
+        for i in range(0,days):
             date = str(sDay+i)+'-'+str(sMonth)+'-'+str(sYear)
             x = requests.get('https://api.coingecko.com/api/v3/coins/'+coins[coin]+'/history?date=' +date+'&localization=usd' + '?format=json' )
             check = x.json()
             price = check["market_data"]["current_price"]['usd']
-            #priceDict[date] = price
-
-            tmpPrices.append(price)
-            tmpDates.append(date)
-
-        prices.append(tmpPrices)
-        dates.append(tmpDates)
-        #print(priceDict)
-        #coinPrices.append(priceDict)
+            df = df.append({'Date' : date, coins[0]:price},
+                            ignore_index = True)
+    return df
         
-    print(prices)
-    print(dates)
-
-def priceDataPoly(coins, sDay, sMonth, sYear):
-    
-    for i in range(0,1):
-        if(sDay > 9):
-            date = str(sYear)+'-'+str(sMonth)+'-'+str(sDay)
-        else:
-            date = str(sYear)+'-'+str(sMonth)+'-'+'0'+ str(sDay)
-        x = requests.get('https://api.polygon.io/v1/open-close/crypto/ETH/USD/'+date+'?adjusted=false&apiKey='+pApi)
-        check = x.json()
-        print(check["day"])
-        print(check["close"])
 
 def main():
     coins = ['joe', 'bitcoin']
     sDay = 1
     sMonth = 11
     sYear = 2021
-    priceDataCG(coins, sDay, sMonth, sYear)
+    days = 5
+    print (priceDataCG(coins, sDay, sMonth, sYear, days))
 
 main()
